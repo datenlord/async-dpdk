6ebf6ca2171b5778732c197606ec790857584a29
Jingjia Luo <jingjia_luo@163.com>
2022-10-29 20:28:58 +0800

    test: add CI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # run as root
-mkdir -p /dev/hugepages
-mountpoint -q /dev/hugepages || mount -t hugetlbfs nodev /dev/hugepages
-echo 1 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
 
-chmod 600 /sys/bus/pci/drivers/e1000/bind
-chmod 600 /sys/bus/pci/drivers/e1000/unbind
-chmod 600 /sys/bus/pci/drivers/vfio-pci/bind
-chmod 600 /sys/bus/pci/drivers/vfio-pci/unbind
+function setup() {
+    mkdir -p /dev/hugepages
+    mountpoint -q /dev/hugepages || mount -t hugetlbfs nodev /dev/hugepages
+    echo 1 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
 
-modprobe vfio vfio-pci
-echo 1 > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
-# modprobe vfio enable_unsafe_noiommu_mode=1
+    chmod 600 /sys/bus/pci/drivers/e1000/bind
+    chmod 600 /sys/bus/pci/drivers/e1000/unbind
+    chmod 600 /sys/bus/pci/drivers/vfio-pci/bind
+    chmod 600 /sys/bus/pci/drivers/vfio-pci/unbind
 
-usertools/dpdk-devbind.py -b=vfio-pci 02:02.0 02:03.0
+    modprobe vfio vfio-pci
+    echo 1 > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
+    # modprobe vfio enable_unsafe_noiommu_mode=1
 
-# ../usertools/dpdk-devbind.py -u 02:02.0 02:03.0
+    usertools/dpdk-devbind.py -b=vfio-pci 02:02.0 02:03.0
+}
+
+function teardown() {
+    usertools/dpdk-devbind.py -u 02:02.0 02:03.0
+}
